69a4ce47a799c6dcf9032f18ad283382299cd1df
Arpan Banerjee <arpanbnrj9@gmail.com>
2017-07-08 00:53:42 +0530

    Added multiple damage types to Resist effect and copper ring (#109)
    * Added multiple damage types to Resist effect and copper ring
    
    * fixed id issue

--- src/main/java/org/terasology/equipment/component/effects/ResistEffectComponent.java
+++ src/main/java/org/terasology/equipment/component/effects/ResistEffectComponent.java
@@ -15,7 +15,16 @@
  */
 package org.terasology.equipment.component.effects;
 
+import org.terasology.alterationEffects.resist.ResistDamageEffect;
 import org.terasology.equipment.component.EquipmentEffectComponent;
+import org.terasology.network.Replicate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ResistEffectComponent extends EquipmentEffectComponent {
+
+    /** Stores the map of damage type class name to resistances. */
+    @Replicate
+    public Map<String, ResistDamageEffect> resistances = new HashMap<>();
 }
